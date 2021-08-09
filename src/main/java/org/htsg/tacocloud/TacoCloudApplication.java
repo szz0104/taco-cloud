46c9655579c732adff47aa0b0159ce0c547e75d7
szhizong <szz0104@hotmail.com>
2021-08-09 11:02:34 +0800

    [新增]项目初始化

---
+++ src/main/java/org/htsg/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,16 @@
+package org.htsg.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 河图书卦
+ */
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+}
